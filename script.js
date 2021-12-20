a5b02c82e05892e2415e85af3814d3ae63a7d5f9
MatVicDev <matheus@teste.com>
2021-12-20 15:27:15 -0300

    Definindo o tamanho da tela

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-window.alert('Teste')
+let altura = 0
+let largura = 0
+
+function tamanhoTela()
+{
+	altura = window.innerHeight
+	largura = window.innerWidth
+
+	console.log(altura, largura)
+}
+
+tamanhoTela()
