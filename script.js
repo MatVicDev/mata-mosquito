3feb07603c241eb485c6c65a82898f79919302c1
MatVicDev <matheus@teste.com>
2021-12-20 16:04:00 -0300

    renderizando o mosquito aleatoriamente

--- script.js
+++ script.js
@@ -6,7 +6,25 @@ function tamanhoTela()
 	altura = window.innerHeight
 	largura = window.innerWidth
 
-	console.log(altura, largura)
+	console.log(largura, altura)
 }
 
 tamanhoTela()
+
+var posicaoX = Math.floor(Math.random() * largura) - 90
+var posicaoY = Math.floor(Math.random() * altura) - 90
+
+posicaoX = posicaoX < 0 ? 0 : posicaoX 
+posicaoY = posicaoY < 0 ? 0 : posicaoY 
+
+console.log(posicaoX, posicaoY)
+
+// Criar o elemento HTML de imagem
+var mosquito = document.createElement('img')
+mosquito.src = 'imagens/mosca.png'
+mosquito.className = 'mosquito1'
+mosquito.style.position = 'absolute'
+mosquito.style.left = posicaoX + 'px'
+mosquito.style.top = posicaoY + 'px'
+
+document.body.appendChild(mosquito)
