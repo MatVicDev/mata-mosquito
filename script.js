059ac0f5367eecdec37f756ce0f1e5c3adeabee5
MatVicDev <matheus@teste.com>
2021-12-22 15:47:39 -0300

    Finalizando o projeto

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 let altura = 0
 let largura = 0
 let vidas = 1
-let tempo = 10
+let tempo = 60
+
+let velocidadeMosquito = 1500
+
+let nivel = window.location.search.replace('?nivel=', '')
+
+if(nivel === 'normal') {
+	velocidadeMosquito = 1500
+} else if(nivel === 'dificil') {
+	velocidadeMosquito = 1000
+} else if(nivel === 'chucknorris') {
+	velocidadeMosquito = 750
+}
+
 
 function tamanhoTela()
 {
